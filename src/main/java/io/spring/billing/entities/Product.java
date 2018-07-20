2de98e323d25787eb386d1a5be7e2a6b13b1d586
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-07-20 18:27:59 +0200

    added lombok equals and hashcode

--- src/main/java/io/spring/billing/entities/Product.java
+++ src/main/java/io/spring/billing/entities/Product.java
@@ -1,6 +1,7 @@
 package io.spring.billing.entities;
 
 import com.sun.istack.internal.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode(of = {"id"})
 public class Product implements BillingEntity {
 
     @Id
