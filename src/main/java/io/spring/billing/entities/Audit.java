10e969e37761ab11f9605f9460ae7de31c5f9c65
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-07-20 18:56:29 +0200

    added lombok getter and setter to audit

--- src/main/java/io/spring/billing/entities/Audit.java
+++ src/main/java/io/spring/billing/entities/Audit.java
@@ -1,10 +1,15 @@
 package io.spring.billing.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Embeddable;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.util.Date;
 
+@Getter
+@Setter
 @Embeddable
 public class Audit {
 
