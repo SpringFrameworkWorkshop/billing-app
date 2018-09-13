b5ec0c000036706fb8d53f3923b9118a2b1e9ca6
Antonio <antoniovizuete@gmail.com>
2018-09-13 13:16:49 +0200

    Primeros Pasos

---
+++ src/main/java/io/spring/billing/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package io.spring.billing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+      .passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
+      .withUser("user1").password("secret1").roles("USER")
+      .and()
+      .withUser("admin1").password("secret1").roles("USER", "ADMIN");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+            .antMatchers("/","/css/**","/js/**","/images/**","/login","/logout").permitAll()
+            .anyRequest().authenticated()
+            .and()
+        .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/clients")
+            .permitAll()
+            .and()
+        .logout()
+            .permitAll();
+  }
+}
--- src/main/java/io/spring/billing/controllers/HomeController.java
+++ src/main/java/io/spring/billing/controllers/HomeController.java
@@ -1,15 +1,25 @@
 package io.spring.billing.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
-        return "redirect:/clients";
+        return "login";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
 }
